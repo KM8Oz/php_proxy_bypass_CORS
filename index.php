1b74a4df8b9acaae14693e844e3e650d7acc415f
kmoz000 <ouazmir@gmail.com>
2018-09-15 00:15:32 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
 <?PHP
+
+   if ($_SERVER['HTTP_ORIGIN']) {
+     
+   // change  $_SERVER['HTTP_ORIGIN'] to any thing you wan th get access to your proxy from a single location or multiple
+     // ############################################################################
+      header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+      header('Access-Control-Allow-Credentials: true');
+      header('Access-Control-Max-Age: 3700');    // cache for 1 hours
+  }
+
+  // Access-Control headers are received during OPTIONS requests
+  if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+      if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+          header("Access-Control-Allow-Methods: POST");         
+
+      if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+          header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+      exit(0);
+  }
 ini_set('memory_limit','105M');
 error_reporting(E_ALL ^ E_NOTICE);
 // ############################################################################
