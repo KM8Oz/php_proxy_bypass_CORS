f5235e6c1ade98daa1f61cc307a00f97ea3159a4
kmoz000 <ouazmir@gmail.com>
2018-09-14 03:33:40 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,120 +1,14 @@
 <?PHP
-isset($_GET['readme']) ? ($_GET['readme'] == 'true' ? 
- print_r('<pre>
- // to see that readme 
-// Certain GET (query string) parameters may be passed into ba-simple-proxy.php
-// to control its behavior, this is a list of these parameters. 
-// 
-//   url - The remote URL resource to fetch. Any GET parameters to be passed
-//     through to the remote URL resource must be urlencoded in this parameter.
-//   mode - If mode=native, the response will be sent using the same content
-//     type and headers that the remote URL resource returned. If omitted, the
-//     response will be JSON (or JSONP). <Native requests> and <JSONP requests>
-//     are disabled by default, see <Configuration Options> for more information.
-//   callback - If specified, the response JSON will be wrapped in this named
-//     function call. This parameter and <JSONP requests> are disabled by
-//     default, see <Configuration Options> for more information.
-//   user_agent - This value will be sent to the remote URL request as the
-//     `User-Agent:` HTTP request header. If omitted, the browser user agent
-//     will be passed through.
-//   send_cookies - If send_cookies=1, all cookies will be forwarded through to
-//     the remote URL request.
-//   send_session - If send_session=1 and send_cookies=1, the SID cookie will be
-//     forwarded through to the remote URL request.
-//   full_headers - If a JSON request and full_headers=1, the JSON response will
-//     contain detailed header information.
-//   full_status - If a JSON request and full_status=1, the JSON response will
-//     contain detailed cURL status information, otherwise it will just contain
-//     the `http_code` property.
-// 
-// Topic: POST Parameters
-// 
-// All POST parameters are automatically passed through to the remote URL
-// request.
-// 
-// Topic: JSON requests
-// 
-// This request will return the contents of the specified url in JSON format.
-// 
-// Request:
-// 
-// > proxy.php?url=http://example.com/
-// 
-// Response:
-// 
-// > { "contents": "<html>...</html>", "headers": {...}, "status": {...} }
-// 
-// JSON object properties:
-// 
-//   contents - (String) The contents of the remote URL resource.
-//   headers - (Object) A hash of HTTP headers returned by the remote URL
-//     resource.
-//   status - (Object) A hash of status codes returned by cURL.
-// 
-// Topic: JSONP requests
-// 
-// This request will return the contents of the specified url in JSONP format
-// (but only if $enable_jsonp is enabled in the PHP script).
-// 
-// Request:
-// 
-// > proxy.php?url=http://example.com/&callback=foo
-// 
-// Response:
-// 
-// > foo({ "contents": "<html>...</html>", "headers": {...}, "status": {...} })
-// 
-// JSON object properties:
-// 
-//   contents - (String) The contents of the remote URL resource.
-//   headers - (Object) A hash of HTTP headers returned by the remote URL
-//     resource.
-//   status - (Object) A hash of status codes returned by cURL.
-// 
-// Topic: Native requests
-// 
-// This request will return the contents of the specified url in the format it
-// was received in, including the same content-type and other headers (but only
-// if $enable_native is enabled in the PHP script).
-// 
-// Request:
-// 
-// > ba-simple-proxy.php?url=http://example.com/&mode=native
-// 
-// Response:
-// 
-// > <html>...</html>
-// 
-// Topic: Notes
-// 
-// * Assumes magic_quotes_gpc = Off in php.ini
-// 
-// Topic: Configuration Options
-// 
-// These variables can be manually edited in the PHP file if necessary.
-// 
-//   $enable_jsonp - Only enable <JSONP requests> if you really need to. If you
-//     install this script on the same server as the page you'.'re calling it
-//     from, plain JSON will work. Defaults to false.
-//   $enable_native - You can enable <Native requests>, but you should only do
-//     this if you also whitelist specific URLs using $valid_url_regex, to avoid
-//     possible XSS vulnerabilities. Defaults to false.
-//   $valid_url_regex - This regex is matched against the url parameter to
-//     ensure that it is valid. This setting only needs to be used if either
-//     $enable_jsonp or $enable_native are enabled. Defaults to /.*/ which
-//     validates all URLs.
-// 
-// ############################################################################') : print("### set readme to: 'true'") ) : null ;
 
-// Change these configuration options if needed, see above descriptions for info.
-$enable_jsonp    = false;
 $enable_native   = false;
 $valid_url_regex = '/.*/';
 
-// ############################################################################
+// #######################################fb.me/kmoz000##################################
 
-$url = $_GET['url'];
-
+$url = isset($_GET['url']) ? $_GET['url'] : false ;
+$type = isset($_GET['type']) ? $_GET['type'] : false;
+$url = isset($_POST['url']) ? $_POST['url'] : false ;
+$type = isset($_POST['type']) ? $_POST['type'] : false;
 if ( !$url ) {
   
   // Passed url not specified.
@@ -134,20 +28,7 @@ if ( !$url ) {
     curl_setopt( $ch, CURLOPT_POST, true );
     curl_setopt( $ch, CURLOPT_POSTFIELDS, $_POST );
   }
-  
-  if ( isset($_GET['send_cookies']) ) {
-    $cookie = array();
-    foreach ( $_COOKIE as $key => $value ) {
-      $cookie[] = $key . '=' . $value;
-    }
-    if ( isset($_GET['send_session']) ) {
-      $cookie[] = SID;
-    }
-    $cookie = implode( '; ', $cookie );
-    
-    curl_setopt( $ch, CURLOPT_COOKIE, $cookie );
-  }
-  
+
   curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true );
   curl_setopt( $ch, CURLOPT_HEADER, true );
   curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
@@ -163,13 +44,13 @@ if ( !$url ) {
 
 // Split header text into an array.
 $header_text = preg_split( '/[\r\n]+/', $header );
-
-if ( isset($_GET['mode']) ) {
-  if ( !($_GET['mode'] == 'native') ) {
-    $contents = 'ERROR: invalid mode';
-    $status = array( 'http_code' => 'ERROR' );
-  }
-  
+$jsonresp =false;
+if ( !($type == 'html') ) {
+  $type == 'json' ? $jsonresp = true : (
+  $contents = 'ERROR: invalid mode'&&
+  $status = array( 'http_code' => 'ERROR' ));
+}
+if ($type == 'html') {
   // Propagate headers to response.
   foreach ( $header_text as $header ) {
     if ( preg_match( '/^(?:Content-Type|Content-Language|Set-Cookie):/i', $header ) ) {
@@ -180,31 +61,10 @@ if ( isset($_GET['mode']) ) {
   print $contents;
   
 } else {
-  
-  // $data will be serialized into JSON data.
+
   $data = array();
-  
-  // Propagate all HTTP headers into the JSON data object.
-  if ( isset($_GET['full_headers']) ) {
-    $data['headers'] = array();
-    
-    foreach ( $header_text as $header ) {
-      preg_match( '/^(.+?):\s+(.*)$/', $header, $matches );
-      if ( $matches ) {
-        $data['headers'][ $matches[1] ] = $matches[2];
-      }
-    }
-  }
-  
-  // Propagate all cURL request / response info to the JSON data object.
-  if ( isset($_GET['full_status']) ) {
-    $data['status'] = $status;
-  } else {
     $data['status'] = array();
     $data['status']['http_code'] = $status['http_code'];
-  }
-  
-  // Set the JSON data object contents, decoding it from JSON if possible.
   $decoded_json = json_decode( $contents );
   $dom = new DOMDocument;
   function element_to_obj($element) {
@@ -213,32 +73,21 @@ if ( isset($_GET['mode']) ) {
         $obj[$attribute->name] = $attribute->value;
     }
     foreach ($element->childNodes as $subElement) {
-        if ($subElement->nodeType == XML_TEXT_NODE) {
-            $obj["html"] = $subElement->wholeText;
-        }
-        else{
-            $obj["children"][] = element_to_obj($subElement);
-        }
+       ($subElement->nodeType == XML_TEXT_NODE) ? 
+            $obj["html"] = $subElement->wholeText : $obj["children"][] = element_to_obj($subElement);
     }
     return $obj;
 }
   $data['contents'] = $decoded_json ? $decoded_json :  ($dom->loadHTML($contents) ? element_to_obj($dom->documentElement) : false );
   
-  // Generate appropriate content-type header.
 ( isset($_SERVER['HTTP_X_REQUESTED_WITH']) ) ? 
   $is_xhr = strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest' &&
   header( 'Content-type: application/' . ( $is_xhr ? 'json' : 'x-javascript' ) ) 
 :  null ;
 
- 
-  
-  // Get JSONP callback.
-  $jsonp_callback = $enable_jsonp && isset($_GET['callback']) ? $_GET['callback'] : null;
-  
-  // Generate JSON/JSONP string
   $json = json_encode( $data );
   
-  print_r($jsonp_callback ? "$jsonp_callback($json)" : $json);
+  print_r($jsonresp ? $json : $data);
   
 }
 
